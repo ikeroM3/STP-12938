3074c8a5f910961b49f54a0e00004202391fcfa7
Artem <stuzukartem12@gmail.com>
2026-06-05 15:29:04 +0300

    fix header

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -7,11 +7,11 @@ burgerBtn.addEventListener('click', () => {
 
   if (isOpen) {
     burgerOverlay.dataset.visible = 'close';
-    burgerUse.setAttribute('href', '/img/svg/sprite.svg#icon-icon-burger');
+    burgerUse.setAttribute('href', 'img/svg/sprite.svg#icon-icon-burger');
     burgerBtn.classList.remove('is-open');
   } else {
     burgerOverlay.dataset.visible = 'open';
-    burgerUse.setAttribute('href', '/img/svg/sprite.svg#icon-icon-close');
+    burgerUse.setAttribute('href', 'img/svg/sprite.svg#icon-icon-close');
     burgerBtn.classList.add('is-open');
   }
 });

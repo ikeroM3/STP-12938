78bbab78fc245f01b7571a963efe2d0bc14fc641
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-06-08 19:49:38 +0300

    Refactor FAQ JS to use data attributes

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,34 +1,41 @@
 import spritePath from './../img/svg/sprite.svg';
-const faqButtons = document.querySelectorAll('.faq-question');
+
+const faqItems = document.querySelectorAll('[data-faq-item]');
+const faqButtons = document.querySelectorAll('[data-faq-button]');
+
+faqItems.forEach(item => {
+  const answer = item.querySelector('[data-faq-answer]');
+
+  if (item.dataset.open === 'true') {
+    answer.style.maxHeight = `${answer.scrollHeight}px`;
+  } else {
+    answer.style.maxHeight = '0px';
+  }
+});
 
 faqButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const currentItem = button.closest('.faq-item');
-    const currentAnswer = currentItem.querySelector('.faq-answer');
-    const currentIconUse = currentItem.querySelector('.faq-icon-svg use');
+    const currentItem = button.closest('[data-faq-item]');
+    const currentAnswer = currentItem.querySelector('[data-faq-answer]');
+    const currentIconUse = currentItem.querySelector('[data-faq-icon]');
+    const isOpen = currentItem.dataset.open === 'true';
 
-    document.querySelectorAll('.faq-item').forEach(item => {
-      const answer = item.querySelector('.faq-answer');
-      const iconUse = item.querySelector('.faq-icon-svg use');
+    faqItems.forEach(item => {
+      const answer = item.querySelector('[data-faq-answer]');
+      const iconUse = item.querySelector('[data-faq-icon]');
 
-      if (item !== currentItem) {
-        item.classList.remove('faq-item--open');
-        answer.style.maxHeight = '0px';
-        iconUse.setAttribute('href', `${spritePath}#icon-faq-plus-circle`);
-      }
+      item.dataset.open = 'false';
+      answer.style.maxHeight = '0px';
+      iconUse.setAttribute('href', `${spritePath}#icon-faq-plus-circle`);
     });
 
-    currentItem.classList.toggle('faq-item--open');
-
-    if (currentItem.classList.contains('faq-item--open')) {
-      currentAnswer.style.maxHeight = currentAnswer.scrollHeight + 'px';
+    if (!isOpen) {
+      currentItem.dataset.open = 'true';
+      currentAnswer.style.maxHeight = `${currentAnswer.scrollHeight}px`;
       currentIconUse.setAttribute(
         'href',
         `${spritePath}#icon-faq-close-circle`
       );
-    } else {
-      currentAnswer.style.maxHeight = '0px';
-      currentIconUse.setAttribute('href', `${spritePath}#icon-faq-plus-circle`);
     }
   });
 });
